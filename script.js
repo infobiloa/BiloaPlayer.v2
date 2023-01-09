988dfe38b27eb6e75188ec84f0bbbf08ae98c09c
infobiloa <109538180+infobiloa@users.noreply.github.com>
2023-01-08 19:36:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+function getparam(a,e){return e||(e=window.location.href),new URL(e).searchParams.get(a)}
+let s=a=>document.getElementById(a);
+
+let get=()=>{
+  getbase();
+}
+let getbase=()=>{
+    let ply = [];
+  
+  
+    ply.id =  s('videourl').value;
+      
+   
+    console.log(ply);
+  
+    var arr = JSON.stringify(Object.assign({}, ply))
+    console.log(btoa(arr));
+    window.base =btoa(arr);
+    iframe();
+    return btoa(arr);
+}
+
+
+
+let openplyr=()=> { window.open('plyr.html?id='+base)}
+
+
+let pmsg = 'Copy Embed Code' ;
+//Player Embedders
+
+let embedplyr=()=> {
+  prompt(pmsg,`<iframe width="560" height="315" 
+scrolling="no"
+src="plyr.html?id=${base}" 
+frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+gyroscope; picture-in-picture" allowfullscreen>
+</iframe>`)
+}
+
+
+
+
+let iframe=()=> {
+    s('plyr').src='plyr.html?id='+base;
+  
+  
+}
